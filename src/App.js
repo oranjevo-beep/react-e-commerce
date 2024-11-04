04d32b9a93458eadb49524515a098e31745c6828
Denys <oranjevo@gmail.com>
2024-11-04 20:17:16 +0800

    first directory update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import categoriesData from './categories.json';
+import Categories from './components/categories/Categories';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Categories categories={categoriesData} />;
 }
 
 export default App;
